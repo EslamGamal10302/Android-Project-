ea17103c18d338edc222fbac6f8c126e12920909
eslam <eslam10302@gmail.com>
2023-02-08 22:14:12 +0200

    calendar screen

--- Project/app/src/main/java/com/example/project/home/CalendarActivity.java
+++ Project/app/src/main/java/com/example/project/home/CalendarActivity.java
@@ -2,6 +2,8 @@ package com.example.project.home;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,11 +13,147 @@ import com.example.project.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class CalendarActivity extends AppCompatActivity {
+    RecyclerView saturdayRecycle;
+    CalendarAdapter saturdayAdapter;
 
+    LinearLayoutManager myManger;
+
+
+    RecyclerView sundayRecycle;
+    CalendarAdapter sundayAdapter;
+
+    LinearLayoutManager sundayManger;
+    RecyclerView mondayRecycle;
+    CalendarAdapter monddayAdapter;
+
+    LinearLayoutManager mondayManger;
+
+    RecyclerView tuesdaydayRecycle;
+
+    CalendarAdapter tuesdaydayAdapter;
+
+    LinearLayoutManager tuesdaydayManger;
+
+
+    RecyclerView wednesdayRecycle;
+    CalendarAdapter wednesdayAdapter;
+
+    LinearLayoutManager wednesdayManger;
+
+
+    RecyclerView thursdaydayRecycle;
+    CalendarAdapter thursdaydayAdapter;
+
+    LinearLayoutManager thursdaydayManger;
+
+
+
+    RecyclerView fridayRecycle;
+    CalendarAdapter fridayAdapter;
+
+    LinearLayoutManager fridayManger;
+
+    Calendar [] saturday = {new Calendar("beef",R.drawable.egypt),new Calendar("burger",R.drawable.malysia)};
+
+    Calendar [] sunday = {new Calendar("chicken",R.drawable.england),new Calendar("eslam",R.drawable.netherland)};
+
+    Calendar []  monday = {new Calendar("molokhia",R.drawable.tunisa),new Calendar("melad",R.drawable.canada)};
+
+    Calendar []  tuesday = {new Calendar("fata",R.drawable.turkey),new Calendar("melad",R.drawable.canada)};
+
+    Calendar []  wednesday = {new Calendar("stribs",R.drawable.canada),new Calendar("melad",R.drawable.canada)};
+
+    Calendar []  thuresday = {new Calendar("rice",R.drawable.allmeals),new Calendar("melad",R.drawable.canada)};
+
+    Calendar []  friday = {new Calendar("botatos",R.drawable.chinese),new Calendar("melad",R.drawable.canada)};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+        saturdayRecycle = findViewById(R.id.saturday);
+        myManger = new LinearLayoutManager(this);
+        myManger.setOrientation(RecyclerView.HORIZONTAL);
+        saturdayRecycle.setLayoutManager(myManger);
+        saturdayAdapter = new CalendarAdapter(saturday,this );
+        saturdayRecycle.setAdapter(saturdayAdapter);
+
+
+
+        sundayManger = new LinearLayoutManager(this);
+        sundayManger.setOrientation(RecyclerView.HORIZONTAL);
+        sundayRecycle=findViewById(R.id.sunday);
+        sundayRecycle.setLayoutManager(sundayManger);
+        sundayAdapter= new CalendarAdapter(sunday,this);
+        sundayRecycle.setAdapter(sundayAdapter);
+
+
+
+        mondayManger = new LinearLayoutManager(this);
+        mondayManger.setOrientation(RecyclerView.HORIZONTAL);
+        mondayRecycle=findViewById(R.id.monday);
+        mondayRecycle.setLayoutManager(mondayManger);
+        monddayAdapter= new CalendarAdapter(monday,this);
+        mondayRecycle.setAdapter(monddayAdapter);
+
+
+
+        tuesdaydayManger = new LinearLayoutManager(this);
+        tuesdaydayManger.setOrientation(RecyclerView.HORIZONTAL);
+        tuesdaydayRecycle=findViewById(R.id.tuesday);
+        tuesdaydayRecycle.setLayoutManager(tuesdaydayManger);
+        tuesdaydayAdapter= new CalendarAdapter(tuesday,this);
+        tuesdaydayRecycle.setAdapter(tuesdaydayAdapter);
+
+
+
+         wednesdayManger= new LinearLayoutManager(this);
+        wednesdayManger.setOrientation(RecyclerView.HORIZONTAL);
+        wednesdayRecycle=findViewById(R.id.wednesday);
+        wednesdayRecycle.setLayoutManager(wednesdayManger);
+         wednesdayAdapter= new CalendarAdapter(wednesday,this);
+        wednesdayRecycle.setAdapter(wednesdayAdapter);
+
+
+
+
+        thursdaydayManger = new LinearLayoutManager(this);
+        thursdaydayManger.setOrientation(RecyclerView.HORIZONTAL);
+        thursdaydayRecycle=findViewById(R.id.thuresday);
+        thursdaydayRecycle.setLayoutManager(thursdaydayManger);
+        thursdaydayAdapter= new CalendarAdapter(thuresday,this);
+        thursdaydayRecycle.setAdapter(thursdaydayAdapter);
+
+
+
+
+        fridayManger = new LinearLayoutManager(this);
+        fridayManger.setOrientation(RecyclerView.HORIZONTAL);
+        fridayRecycle=findViewById(R.id.friday);
+        fridayRecycle.setLayoutManager(fridayManger);
+        fridayAdapter= new CalendarAdapter(friday,this);
+        fridayRecycle.setAdapter(fridayAdapter);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         BottomNavigationView bottomNavigationView=findViewById(R.id.bottom_navigation);
         bottomNavigationView.setSelectedItemId(R.id.calScreen);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
---
+++ Project/app/src/main/java/com/example/project/home/CalendarAdapter.java
@@ -0,0 +1,63 @@
+package com.example.project.home;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.project.R;
+
+public class CalendarAdapter extends RecyclerView.Adapter <CalendarAdapter.MyViewHolder>{
+    Calendar[] myItems ;
+    Context context;
+
+    public CalendarAdapter(Calendar[] myItems, Context context) {
+        this.myItems = myItems;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public CalendarAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater  = (LayoutInflater) context .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.calendar_view, parent , false);
+        MyViewHolder vh = new MyViewHolder(view);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CalendarAdapter.MyViewHolder holder, int position) {
+        Calendar current = myItems[position];
+        holder.name.setText(current.getName());
+        holder.thumbnails.setImageResource(current.getThumbnail());
+    }
+
+    @Override
+    public int getItemCount() {
+        return myItems.length;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+      //  ConstraintLayout layout;
+        TextView name ;
+
+        ImageView thumbnails;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+          //  layout = itemView.findViewById(R.id.calendar_constrain);
+            name = itemView.findViewById(R.id.tv_random_meals_name);
+            thumbnails = itemView.findViewById(R.id.img_random_meal);
+
+        }
+
+
+    }
+}
