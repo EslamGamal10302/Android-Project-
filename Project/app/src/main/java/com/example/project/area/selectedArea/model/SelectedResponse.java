dd089d634991aa7d914ceceaed2a9dbab186f589
eslam <eslam10302@gmail.com>
2023-02-17 15:40:14 +0200

    fire base DataBase v1

--- Project/app/src/main/java/com/example/project/area/selectedArea/model/SelectedResponse.java
+++ Project/app/src/main/java/com/example/project/area/selectedArea/model/SelectedResponse.java
@@ -1,9 +1,13 @@
 package com.example.project.area.selectedArea.model;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.stream.Collectors;
 
 public class SelectedResponse {
     private ArrayList<Meal> meals ;
+
+
 
     public ArrayList<Meal> getAreaMeals() {
         return meals;
@@ -11,5 +15,16 @@ public class SelectedResponse {
 
     public void setAreaMeals(ArrayList<Meal> areaMeals) {
         this.meals = areaMeals;
+    }
+
+
+
+    public HashMap<String , Meal> toJson(){
+       HashMap<String , Meal>  resultMap = new HashMap<>();
+       for(Meal meal :this.meals){
+           resultMap.put(meal.getIdMeal(),meal);
+       }
+
+        return resultMap;
     }
 }
